1e26f195fcda9917a52e456444d8db55e96a0053
Ernesto Brenes <ernestobrenes96@gmail.com>
2016-09-28 16:13:28 -0600

    Comente la clase principal

--- watsonSoftwareDesignTutorPEDS/src/main/java/logicaDePresentacion/Principal.java
+++ watsonSoftwareDesignTutorPEDS/src/main/java/logicaDePresentacion/Principal.java
@@ -10,9 +10,9 @@ public class Principal
 
 	public static void main(String[] args) {
 		
-		Conversacion p = new Conversacion("35a4cbe1-56ec-4f9e-8b2a-ef80d3a77521","01sumefaccpZ","bea3b85e-8324-47b2-8072-460c6543ab76");
-		p.consultarPregunta("que es abstracción");
-		System.out.println(p.obtenerRespuesta());
+		//Conversacion p = new Conversacion("35a4cbe1-56ec-4f9e-8b2a-ef80d3a77521","01sumefaccpZ","bea3b85e-8324-47b2-8072-460c6543ab76");
+		//p.consultarPregunta("que es abstracción");
+		//System.out.println(p.obtenerRespuesta());
 
 	}
 
